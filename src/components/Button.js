68de44f20cd07066331f1fa99a6b68b78cfccb2f
bazhenak <bazhena.kutergina@gmail.com>
2024-04-28 16:29:36 +0300

    Initialize project using Create React App

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
+
 
 const initialState = () => {
     return 0;
 }
+
+
 const StateComponent = () => {
 
     const [counter, setCounter] = useState(initialState);
-    const [isOpened, setIsOpened] = useState(false);
 
     const handlerSetCounter = () => {
         setCounter(counter + 1);
@@ -16,14 +18,38 @@ const StateComponent = () => {
         setCounter(counter - 1);
     }
 
+    let up = useKeyPress('q')
+    // let down = useKeyPress('w')
+    //
+    if (up)  handlerSetCounter()
+    // if (down) handlerSetCounterMinus()
 
     return (
         <div>
-            <button className={"btn"} onClick={() => handlerSetCounter()}>Click to add</button>
-            <button className={"btn"} onClick={() => handlerSetCounterMinus()}>Click to subtract</button>
+            <button className={"btn"} onClick={handlerSetCounter}>Click to add</button>
+            <button className={"btn"} onClick={handlerSetCounterMinus}>Click to subtract</button>
             <p className={"counter"}>Counter: {counter}</p>
         </div>
     )
 };
-export default StateComponent;
 
+const useKeyPress = (keyTarget) => {
+    const [isPressed, setIsPressed] = useState(false);
+
+    const downHandler = ({key}) => {
+        if (key === keyTarget) setIsPressed(true)
+    }
+
+    useEffect(() => {
+
+        window.addEventListener('keydown', downHandler)
+
+        return () => {
+            window.removeEventListener('keydown', downHandler)
+        }
+    }, [])
+
+    return isPressed
+
+}
+export default StateComponent;
